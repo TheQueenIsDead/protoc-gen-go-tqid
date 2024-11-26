112ca718d9b39d6630634101093f22bf7f428449
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-11-26 21:58:07 +1300

    wip: attempt to parse the ast of the main function template

--- pkg/generate.go
+++ pkg/generate.go
@@ -3,6 +3,9 @@ package pkg
 import (
 	"bytes"
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"google.golang.org/protobuf/compiler/protogen"
 	"log"
 	"strings"
@@ -33,9 +36,31 @@ func Generate(plugin *protogen.Plugin, opts Options) (err error) {
 
 func generateBoilerplate(plugin *protogen.Plugin, opts Options) (err error) {
 
+	// Read main.go and interpolate the servicename where required.
 	buf, err := ReadFsFile(Main)
 	repl := strings.Replace(string(buf), `"template"`, fmt.Sprintf(`"%s"`, opts.ServiceName), -1)
 	buf = []byte(repl)
+
+	// WIP Attempt to parse the given file into it's AST
+	fest := token.NewFileSet()
+	f, err := parser.ParseFile(fest, "", buf, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+	log.Println(f)
+	ast.Inspect(f, func(n ast.Node) bool {
+		funcDecl, ok := n.(*ast.FuncDecl)
+		if ok {
+			if funcDecl.Name.Name == "main" {
+				log.Println("FOUND MAIN:")
+				log.Println(funcDecl.Body.List[0])
+			}
+		}
+		return true
+	})
+	// END: WIP Attempt to parse the given file into it's AST
+
+	// Write a new generated file to the plugin for rendering later
 	file := plugin.NewGeneratedFile("main.go", ".")
 	_, err = file.Write(buf)
 	if err != nil {
